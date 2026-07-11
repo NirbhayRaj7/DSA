af86e2ef59470f0d271832fd45ec56d592add423
NirbhayRaj7 <nirbhayraj986@gmail.com>
2026-07-11 13:39:48 +0530

    Time: 3 ms (52.14%) | Memory: 264 MB (51.34%) - LeetSync

---
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) {
+        int ans = 0;
+        for (int x : derived) {
+            ans ^= x;
+        }
+        return ans == 0;
+    }
+};
